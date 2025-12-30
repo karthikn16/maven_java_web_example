6aa209c6619411fd5d57facfc20b9e9958a766a5
karthikeyan <81953615+karthikn16@users.noreply.github.com>
2025-12-31 01:50:55 +0530

    Rename test method and remove commented code

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,14 +1,10 @@
 import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class HelloTest {
 
     @Test
-    public void hello_first_test_case() {
+    public void testTrue() {
         assertTrue(true);
-//        fail("เขียนชุดการทดสอบสิ");
     }
-
 }
